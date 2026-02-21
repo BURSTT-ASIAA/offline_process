519d8e1ec38b4367fe0f706f1b3578c30d35bdcf
ubuntu@burstt11 <ubuntu@burstt11>
2026-02-21 11:37:38 +0800

    update check_eigenmodes.py

--- plot_eigenmode.py
+++ plot_eigenmode.py
@@ -24,7 +24,7 @@ pg  = inp.pop(0)
 usage = '''
 plot the results from 'check_eigenmodes.py'
 syntax:
-    %s <h5_file> [options]
+    %s <h5_files> [options]
 
 options are:
     -a Aref         # reference antenna for phase plot
@@ -32,13 +32,16 @@ options are:
     -m MODE         # plot the M-th strongest eigenmode
                     # default: %d
     --xt type       # x-axis type:: 1: UT; 2: win_ID
+                    # default: %d
+    --flim MIN MAX  # frequency range in MHz
+                    # default: %.0f %.0f
 
-
-''' % (pg, Aref, emode)
+''' % (pg, Aref, emode, xtype, flim[0], flim[1])
 
 if (len(inp)<1):
     sys.exit(usage)
 
+files = []
 while (inp):
     k = inp.pop(0)
     if (k == '-m'):
@@ -47,164 +50,174 @@ while (inp):
         Aref = int(inp.pop(0))
     elif (k == '--xt'):
         xtype = int(inp.pop(0))
+    elif (k == '--flim'):
+        flim[0] = float(inp.pop(0))
+        flim[1] = float(inp.pop(0))
     elif (k.startswith('-')):
         sys.exit('unknown option: %s'%k)
     else:
         fin = k
+        files.append(fin)
+nFile = len(files)
 
 if (xtype==1):
     xname = 'UT'
 elif (xtype==2):
     xname = 'win'
 
-odir = fin.replace('.h5', '.mode%d.Aref%d.%s.plots'%(emode,Aref,xname))
-if (not os.path.isdir(odir)):
-    call('mkdir -p %s'%odir, shell=True)
-print('output saved in:', odir)
 
 
-attrs = getAttrs(fin)
-epoch0 = attrs['unix_utc_open']
-tsec = getData(fin, 'winSec')
+for fi in range(nFile):
+    fin = files[fi]
 
-if (vmode == 2):
-    savW2 = getData(fin, 'win_W_scale')
-    savV2 = getData(fin, 'win_V_scale')
-    nWin, nChan, nAnt, nMode = savV2.shape
-if (vmode == 3):
-    savW3 = getData(fin, 'win_W_coeff')
-    savV3 = getData(fin, 'win_V_coeff')
-    nWin, nChan, nAnt, nMode = savV3.shape
-
-ut = Time(tsec+epoch0, format='unix').to_datetime()
-
-freq = getData(fin, 'freq')
-if (freq is None):
-    freq = np.linspace(flim[0], flim[1], nChan, endpoint=False)
-
-chan = np.arange(nChan)
-if (xtype==1):
-    tx   = ut
-    xlab = 'time (UT)'
-elif (xtype==2):
-    tx   = np.arange(nWin)
-    xlab = 'win_ID'
-X, Y = np.meshgrid(tx, freq, indexing='xy')
+    odir = fin.replace('.h5', '.mode%d.Aref%d.%s.plots'%(emode,Aref,xname))
+    if (not os.path.isdir(odir)):
+        call('mkdir -p %s'%odir, shell=True)
+    print('output saved in:', odir)
 
 
-if (vmode == 2):
-    V2last  = savV2[:,:,:,-emode]
-    V2ref  = V2last[:,:,Aref]          # arbitrarily choose ant8 as phase ref
-    V2norm = V2ref / np.abs(V2ref)  # keep only the phase info
-    V2rel  = V2last / V2norm.reshape((nWin,nChan,1))  # phase relative to Vref
-if (vmode == 3):
-    V3last  = savV3[:,:,:,-emode]
-    V3ref  = V3last[:,:,Aref]          # arbitrarily choose ant8 as phase ref
-    V3norm = V3ref / np.abs(V3ref)  # keep only the phase info
-    V3rel  = V3last / V3norm.reshape((nWin,nChan,1))  # phase relative to Vref
+    attrs = getAttrs(fin)
+    epoch0 = attrs['unix_utc_open']
+    tsec = getData(fin, 'winSec')
 
-## phase plots
-for pt in range(4):
-    if (pt == 0):
-        if (vmode==2):
-            continue
-        z = np.angle(V3last)
-        png = '%s/eigenvec.coeff.V.phase.png'%odir
-        sptitle = 'file: %s, coeff eigenvector, phase'%fin
-    elif (pt == 1):
-        if (vmode==3):
-            continue
-        z = np.angle(V2last)
-        png = '%s/eigenvec.scale.V.phase.png'%odir
-        sptitle = 'file: %s, scale eigenvector, phase'%fin
-    elif (pt == 2):
-        if (vmode==2):
-            continue
-        z = np.angle(V3rel)
-        png = '%s/eigenvec.coeff.Vrel.phase.png'%odir
-        sptitle = 'file: %s, coeff eigenvector, phase, ref=Ant%d'%(fin,Aref)
-    elif (pt == 3):
-        if (vmode==3):
-            continue
-        z = np.angle(V2rel)
-        png = '%s/eigenvec.scale.Vrel.phase.png'%odir
-        sptitle = 'file: %s, scale eigenvector, phase, ref=Ant%d'%(fin,Aref)
+    if (vmode == 2):
+        savW2 = getData(fin, 'win_W_scale')
+        savV2 = getData(fin, 'win_V_scale')
+        nWin, nChan, nAnt, nMode = savV2.shape
+    if (vmode == 3):
+        savW3 = getData(fin, 'win_W_coeff')
+        savV3 = getData(fin, 'win_V_coeff')
+        nWin, nChan, nAnt, nMode = savV3.shape
 
-    print('plotting:', png)
+    ut = Time(tsec+epoch0, format='unix').to_datetime()
 
-    fig, s2d = plt.subplots(4,4,figsize=(16,12), sharex=True, sharey=True)
-    sub = s2d.flatten()
+    freq = getData(fin, 'freq')
+    if (freq is None):
+        freq = np.linspace(flim[0], flim[1], nChan, endpoint=False)
 
-    #for ai in range(nAnt):
-    ai = -1
-    for ii in range(4):
-        for jj in range(4):
-            ai += 1
-            ax = sub[ai]
-            ax.pcolormesh(X,Y,z[:,:,ai].T, vmin=-3.2, vmax=3.2, shading='nearest')
-            ax.set_title('ant%02d'%ai)
-            if (ii==3):
-                #ax.set_xlabel('window_id')
-                ax.set_xlabel(xlab)
-            if (jj==0):
-                ax.set_ylabel('freq (MNhz)')
-        
-    sptitle += ' zlim:[%.1f, %.1f]'%(-3.2, 3.2)
-
+    chan = np.arange(nChan)
     if (xtype==1):
-        fig.autofmt_xdate()
-    fig.tight_layout(rect=[0,0.03,1,0.95])
-    fig.subplots_adjust(wspace=0., hspace=0.1)
-    fig.suptitle(sptitle)
-    fig.savefig(png)
-    plt.close(fig)
+        tx   = ut
+        xlab = 'time (UT)'
+    elif (xtype==2):
+        tx   = np.arange(nWin)
+        xlab = 'win_ID'
+    X, Y = np.meshgrid(tx, freq, indexing='xy')
+
+
+    if (vmode == 2):
+        V2last  = savV2[:,:,:,-emode]
+        V2ref  = V2last[:,:,Aref]          # arbitrarily choose ant8 as phase ref
+        V2norm = V2ref / np.abs(V2ref)  # keep only the phase info
+        V2rel  = V2last / V2norm.reshape((nWin,nChan,1))  # phase relative to Vref
+    if (vmode == 3):
+        V3last  = savV3[:,:,:,-emode]
+        V3ref  = V3last[:,:,Aref]          # arbitrarily choose ant8 as phase ref
+        V3norm = V3ref / np.abs(V3ref)  # keep only the phase info
+        V3rel  = V3last / V3norm.reshape((nWin,nChan,1))  # phase relative to Vref
+
+    ## phase plots
+    for pt in range(4):
+        if (pt == 0):
+            if (vmode==2):
+                continue
+            z = np.angle(V3last)
+            png = '%s/eigenvec.coeff.V.phase.png'%odir
+            sptitle = 'file: %s, coeff eigenvector, phase'%fin
+        elif (pt == 1):
+            if (vmode==3):
+                continue
+            z = np.angle(V2last)
+            png = '%s/eigenvec.scale.V.phase.png'%odir
+            sptitle = 'file: %s, scale eigenvector, phase'%fin
+        elif (pt == 2):
+            if (vmode==2):
+                continue
+            z = np.angle(V3rel)
+            png = '%s/eigenvec.coeff.Vrel.phase.png'%odir
+            sptitle = 'file: %s, coeff eigenvector, phase, ref=Ant%d'%(fin,Aref)
+        elif (pt == 3):
+            if (vmode==3):
+                continue
+            z = np.angle(V2rel)
+            png = '%s/eigenvec.scale.Vrel.phase.png'%odir
+            sptitle = 'file: %s, scale eigenvector, phase, ref=Ant%d'%(fin,Aref)
+
+        print('plotting:', png)
+
+        fig, s2d = plt.subplots(4,4,figsize=(16,12), sharex=True, sharey=True)
+        sub = s2d.flatten()
+
+        #for ai in range(nAnt):
+        ai = -1
+        for ii in range(4):
+            for jj in range(4):
+                ai += 1
+                ax = sub[ai]
+                ax.pcolormesh(X,Y,z[:,:,ai].T, vmin=-3.2, vmax=3.2, shading='nearest')
+                ax.set_title('ant%02d'%ai)
+                if (ii==3):
+                    #ax.set_xlabel('window_id')
+                    ax.set_xlabel(xlab)
+                if (jj==0):
+                    ax.set_ylabel('freq (MNhz)')
+            
+        sptitle += ' zlim:[%.1f, %.1f]'%(-3.2, 3.2)
+
+        if (xtype==1):
+            fig.autofmt_xdate()
+        fig.tight_layout(rect=[0,0.03,1,0.95])
+        fig.subplots_adjust(wspace=0., hspace=0.1)
+        fig.suptitle(sptitle)
+        fig.savefig(png)
+        plt.close(fig)
 
 
 
-## ampld plots
-for pt in range(2):
-    if (pt == 0):
-        if (vmode==2):
-            continue
-        z = np.abs(V3last) * np.sqrt(savW3[:,:,-1]).reshape((nWin,nChan,1))
-        png = '%s/eigenvec.coeff.V.ampld.png'%odir
-        sptitle = 'file: %s, coeff eigenvector, ampld'%fin
-        zmin = 0
-        zmax = 0.6
-    elif (pt == 1):
-        if (vmode==3):
-            continue
-        z = np.abs(V2last) * np.sqrt(savW2[:,:,-1]).reshape((nWin,nChan,1))
-        png = '%s/eigenvec.scale.V.ampld.png'%odir
-        sptitle = 'file: %s, scale eigenvector, ampld'%fin
-        zmin = z.min()
-        zmax = z.max()/10
+    ## ampld plots
+    for pt in range(2):
+        if (pt == 0):
+            if (vmode==2):
+                continue
+            z = np.abs(V3last) * np.sqrt(savW3[:,:,-1]).reshape((nWin,nChan,1))
+            png = '%s/eigenvec.coeff.V.ampld.png'%odir
+            sptitle = 'file: %s, coeff eigenvector, ampld'%fin
+            zmin = 0
+            zmax = 0.6
+        elif (pt == 1):
+            if (vmode==3):
+                continue
+            z = np.abs(V2last) * np.sqrt(savW2[:,:,-1]).reshape((nWin,nChan,1))
+            png = '%s/eigenvec.scale.V.ampld.png'%odir
+            sptitle = 'file: %s, scale eigenvector, ampld'%fin
+            zmin = z.min()
+            zmax = z.max()/10
 
-    print('plotting:', png)
-    print('zmin, zmax:', zmin, zmax)
-    fig, s2d = plt.subplots(4,4,figsize=(16,12), sharex=True, sharey=True)
-    sub = s2d.flatten()
+        print('plotting:', png)
+        print('zmin, zmax:', zmin, zmax)
+        fig, s2d = plt.subplots(4,4,figsize=(16,12), sharex=True, sharey=True)
+        sub = s2d.flatten()
 
-    #for ai in range(nAnt):
-    ai = -1
-    for ii in range(4):
-        for jj in range(4):
-            ai += 1
-            ax = sub[ai]
-            ax.pcolormesh(X,Y,z[:,:,ai].T, vmin=zmin, vmax=zmax, shading='nearest')
-            ax.set_title('ant%02d'%ai)
-            if (ii==3):
-                #ax.set_xlabel('window_id')
-                ax.set_xlabel(xlab)
-            if (jj==0):
-                ax.set_ylabel('freq (MNhz)')
-        
-    sptitle += ' zlim:[%.1f, %.1f]'%(zmin, zmax)
+        #for ai in range(nAnt):
+        ai = -1
+        for ii in range(4):
+            for jj in range(4):
+                ai += 1
+                ax = sub[ai]
+                ax.pcolormesh(X,Y,z[:,:,ai].T, vmin=zmin, vmax=zmax, shading='nearest')
+                ax.set_title('ant%02d'%ai)
+                if (ii==3):
+                    #ax.set_xlabel('window_id')
+                    ax.set_xlabel(xlab)
+                if (jj==0):
+                    ax.set_ylabel('freq (MNhz)')
+            
+        sptitle += ' zlim:[%.1f, %.1f]'%(zmin, zmax)
 
-    if (xtype==1):
-        fig.autofmt_xdate()
-    fig.tight_layout(rect=[0,0.03,1,0.95])
-    fig.subplots_adjust(wspace=0., hspace=0.1)
-    fig.suptitle(sptitle)
-    fig.savefig(png)
-    plt.close(fig)
+        if (xtype==1):
+            fig.autofmt_xdate()
+        fig.tight_layout(rect=[0,0.03,1,0.95])
+        fig.subplots_adjust(wspace=0., hspace=0.1)
+        fig.suptitle(sptitle)
+        fig.savefig(png)
+        plt.close(fig)
